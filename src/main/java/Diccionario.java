cef47790afb9b58112ed52a4517a539b628017b8
diegofdxz <113787026+diegofdxz@users.noreply.github.com>
2023-03-06 22:16:40 +0100

    Initial commit

---
+++ src/main/java/Diccionario.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class Diccionario {
+
+    private HashMap<Character, LinkedList<String>> tablaHash;
+
+    public Diccionario() {
+        tablaHash = new HashMap<>();
+        for (char c = 'a'; c <= 'z'; c++) {
+            tablaHash.put(c, new LinkedList<String>());
+        }
+    }
+
+    public void agregarPalabra(String palabra) {
+        char primeraLetra = palabra.charAt(0);
+        LinkedList<String> palabrasConLetra = tablaHash.get(primeraLetra);
+        palabrasConLetra.add(palabra);
+    }
+
+    public boolean buscarPalabra(String palabra) {
+        char primeraLetra = palabra.charAt(0);
+        LinkedList<String> palabrasConLetra = tablaHash.get(primeraLetra);
+        return palabrasConLetra.contains(palabra);
+    }
+
+    // Aquí podrías incluir la función de lectura del archivo CSV o de texto
+
+}
